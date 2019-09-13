95f105a19b3370de1bcc7afabc0a711bbf266b9e
Arthur Castro <arthur.castro07@gmail.com>
2019-09-13 15:13:36 -0300

    Determinar Determinantes (Ainda não funcional)

---
+++ Projeto-Determinante/README.md
@@ -0,0 +1,2 @@
+# **Projeto Determinates** (1 ponto)
+Determinar o determinante de uma matriz quadrada. A dimensão da matriz é arbitrária. 
---
+++ Projeto-Determinante/main.c
@@ -0,0 +1,150 @@
+// Determinar Determinante de uma Matriz quadrada de qualquer ordem
+// Arthur Castro
+// Setembro de 2019
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Definição de variáveis globais ==============================================================
+int ordemMat;
+float **matriz;
+
+// Declaração de protótipos das funções =========================================================
+void apresentacao();
+void cabecalho();
+void defOrdem();
+float **alocacaoDinamica();   
+void matrizDemo();
+void leitura();
+void impressao();
+void determinante();
+float detLaplace();
+
+// Função Principal - main() ====================================================================
+int main(){
+    apresentacao();
+    defOrdem();
+    matriz = alocacaoDinamica();
+    leitura();
+    impressao();
+    determinante();
+    return 0;
+}
+
+void determinante(){
+    float determ;
+
+    cabecalho();
+    
+    determ = detLaplace();
+
+    printf("\t\xAF O determinante da matriz\n");
+    impressao();
+    printf("\n\t e igual a: \n");
+    printf("\n det(M) = %.2f \n", determ);
+
+}
+
+// Apresentação =================================================================================
+void apresentacao(){
+    system("cls");
+    printf("================================================================================\n");
+    printf("|                                                                              |\n");
+    printf("|                                                                              |\n");
+    printf("|                         LEITURA E IMPRESSAO DE MATRIZ                        |\n");
+    printf("|                          QUADRADA DE QUALQUER ORDEM                          |\n");
+    printf("|                                                                              |\n");
+    printf("|                                                                              |\n");
+    printf("|                   ----------------------------------------                   |\n");
+    printf("|                                                                              |\n");
+    printf("|                                                                              |\n");
+    printf("|                            Arthur Lima de Castro                             |\n");
+    printf("|                              Setembro de 2019                                |\n");
+    printf("|                                                                              |\n");
+    printf("================================================================================\n\n");
+    system("pause");
+}
+
+void cabecalho(){
+    system("cls");
+    printf("================================================================================\n");
+    printf("|           LEITURA E IMPRESSAO DE MATRIZ QUADRADA DE QUALQUER ORDEM           |\n");
+    printf("================================================================================\n\n");
+}
+
+// Definir a Ordem da Matriz Quadrada =================================================================
+void defOrdem(){
+    cabecalho();
+
+    printf("\t\xAF Insira a ordem da matriz quadrada: ");
+    scanf("%i", &ordemMat);
+    printf("\n\t");
+    system("pause");
+}
+
+void matrizDemo(){
+    printf("\t     \xBA a11 a12 a13 ... a1n \xBA \n");
+    printf("\t     \xBA a21 a22 a23 ... a2n \xBA \n");
+    printf("\t M = \xBA a31 a32 a33 ... a3n \xBA \n");
+    printf("\t     \xBA ... ... ... ... ... \xBA \n");
+    printf("\t     \xBA am1 am2 am3 ... amn \xBA \n");
+}
+
+// Alocação Dinâmica de Memória para Matriz de qualquer ordem =====================================
+float **alocacaoDinamica(){
+    float **matriz = (float**)malloc(ordemMat * sizeof(float*));
+    for (int i=0; i<(ordemMat); i++){
+        matriz[i]=(float*)malloc(ordemMat * sizeof(float));
+    }
+    return matriz;
+}
+
+// Insrir dados da Matriz =====================================================================
+void leitura(){
+    unsigned int linha, coluna;
+
+    cabecalho();
+
+    matrizDemo();
+    
+    printf("\n\t\xAF Atribua valores respectivos a matriz como no exemplo acima: \n");
+    for (linha=1; linha<=ordemMat; linha++){
+        for (coluna=1; coluna<=ordemMat; coluna++){
+            printf("\t\t> a(%i %i): ", linha, coluna);
+            scanf("%f", &matriz[linha-1][coluna-1]);
+        }        
+    }
+}
+
+// Impressão da Matriz ========================================================================
+void impressao(){
+    unsigned int linha, coluna;
+
+    printf("\t\xBA");
+    for (linha=1; linha<=ordemMat; linha++){
+       for (coluna=1; coluna<=ordemMat; coluna++){
+            printf("%.2f ", matriz[linha-1][coluna-1]);
+        }
+        if ((coluna > ordemMat) && (linha != ordemMat)){
+            printf("\xBA\n\t\xBA");
+        } else {
+            printf("\xBA\n");
+        }
+    }
+    printf("\n\t");
+    
+   system("pause");
+}
+
+float detLaplace(){
+    float detM = 0;
+    unsigned int linha1 = 0, coluna;
+    
+    for(coluna=1; coluna<=ordemMat; coluna++){
+        coluna = coluna -1;
+
+        detM += (matriz[linha1][coluna]) * ((-1)^(1 + coluna )) * (cofator[linha1][coluna]);
+    }
+
+    return detM;
+}
