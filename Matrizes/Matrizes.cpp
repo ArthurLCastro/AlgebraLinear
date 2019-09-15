343496772adee635d1981d26c7b559d3e7073f6e
Arthur Castro <arthur.castro07@gmail.com>
2019-09-15 00:13:26 -0300

    Preparacao do codigo para criacao da funcao para resolver determinante

--- Matrizes/Matrizes.cpp
+++ Matrizes/Matrizes.cpp
@@ -12,7 +12,9 @@ class Matrizes{
         Matrizes();
 		Matrizes(unsigned int, unsigned int);
         void dimensoes(unsigned int, unsigned int);
+        bool quadrada();
         void setMatriz();
+        float determinante();
         void imprime();
         void imprimeFormatada();
 };
@@ -42,6 +44,14 @@ void Matrizes::dimensoes(unsigned int qtdLinhas, unsigned int qtdColunas){
     mat = matriz;
 }
 
+bool Matrizes::quadrada(){
+    if(numLinhas == numColunas){
+        return true;
+    } else {
+        return false;
+    }
+}
+
 void Matrizes::setMatriz(){         // Inserir dados da Matriz
     for (int linha=1; linha<=numLinhas; linha++){
         for (int coluna=1; coluna<=numColunas; coluna++){
@@ -49,6 +59,10 @@ void Matrizes::setMatriz(){         // Inserir dados da Matriz
             cin >> mat[linha-1][coluna-1];
         }        
     }
+}
+
+float Matrizes::determinante(){
+    return 3;
 }
 
 void Matrizes::imprime(){
@@ -79,19 +93,82 @@ void Matrizes::imprimeFormatada(){
 //===================================== MAIN() =====================================
 unsigned int line, columns;
 
+void apresentacao();
+void cabecalho();
+void matrizDemo();
+
 Matrizes minhaMatriz;
 
 int main() {
-    cout << "\n\xAF Insira a quantidade de Linhas da Matriz: ";
-    cin >> line; 
-    cout << "\xAF Insira a quantidade de Colunas da Matriz: ";
-    cin >> columns; 
+    // Apresentação inicial
+    apresentacao();
+
+    // Definição das dimensões da matriz
+    cabecalho();
+    cout << "\n\t\xAF Insira a quantidade de Linhas da Matriz: ";
+    cin >> line;
+    cout << "\t\xAF Insira a quantidade de Colunas da Matriz: ";
+    cin >> columns;
     minhaMatriz.dimensoes(line, columns);
-    minhaMatriz.setMatriz();
-    minhaMatriz.imprimeFormatada();
+
+    cabecalho();
+    if (minhaMatriz.quadrada()){                // Verifica se a matriz é quadrada
+        // Insere valores à matriz
+        cout << "\n\t\xAF Insira os valores da matriz como a do exemplo a seguir:\n\n";
+        matrizDemo();
+        cout << "\n";
+        minhaMatriz.setMatriz();
     
-    cout << "\n";
-    system("pause");
+        // Calcula e imprime o determinante e a matriz
+        cabecalho();
+        cout << "\n";
+        minhaMatriz.imprimeFormatada();
+        cout << "\n\t\xAF A matriz possui determinante\n\t det(M) = " << minhaMatriz.determinante() << "\n\n\t";
+        system("pause");
+
+    } else {
+        // Imprime mensagem de erro
+        cout << "\n\tMATRIZ INVALIDA!";
+        cout << "\n\tInsira uma matriz quadrada! (Ex: n x n)\n\n\t";
+        system("pause");
+        return 0;
+    }
 
     return 0;
+}
+
+// Funções Auxiliares
+void apresentacao(){
+    system("cls");
+    cout << "================================================================================\n";
+    cout << "|                                                                              |\n";
+    cout << "|                                                                              |\n";
+    cout << "|                            DETERMINANTE DE MATRIZ                            |\n";
+    cout << "|                          QUADRADA DE QUALQUER ORDEM                          |\n";
+    cout << "|                                                                              |\n";
+    cout << "|                                                                              |\n";
+    cout << "|                   ----------------------------------------                   |\n";
+    cout << "|                                                                              |\n";
+    cout << "|                            Arthur Lima de Castro                             |\n";
+    cout << "|                              Setembro de 2019                                |\n";
+    cout << "|                           Algebra Linear - POLI/UPE                          |\n";
+    cout << "|                     Prof.: Paulo Hugo Espirito Santo Lima                    |\n";
+    cout << "|                                                                              |\n";
+    cout << "================================================================================\n\n";
+    system("pause");
+}
+
+void cabecalho(){
+    system("cls");
+    cout << "================================================================================\n";
+    cout << "|               DETERMINANTE DE MATRIZ QUADRADA DE QUALQUER ORDEM              |\n";
+    cout << "================================================================================\n\n";
+}
+
+void matrizDemo(){
+    cout << "\t     \xBA a11 a12 a13 ... a1n \xBA \n";
+    cout << "\t     \xBA a21 a22 a23 ... a2n \xBA \n";
+    cout << "\t M = \xBA a31 a32 a33 ... a3n \xBA \n";
+    cout << "\t     \xBA ... ... ... ... ... \xBA \n";
+    cout << "\t     \xBA am1 am2 am3 ... amn \xBA \n";
 }
