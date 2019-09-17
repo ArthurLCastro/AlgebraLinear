d42e5c3561392040aa701c3ba62d4e8bf352625e
Arthur Castro <arthur.castro07@gmail.com>
2019-09-16 23:34:09 -0300

    Separacao de arquivos 'main.cpp' e 'Matrizes'

---
+++ Matrizes/Matrizes.h
@@ -0,0 +1,32 @@
+#ifndef Matrizes_h
+#define Matrizes_h
+
+// #pragma once
+
+#include <iostream>
+using namespace std;
+
+class Matrizes{
+	private:
+        float** mat = NULL;
+        unsigned int numLinhas = 0, numColunas = 0;
+        unsigned int ordemMatQuad = 0;
+        // void diminuirMatriz(unsigned int, unsigned int);
+        float calcCofator(unsigned int, unsigned int);
+ 
+ 	public:
+        Matrizes();
+        Matrizes(unsigned int ordem);
+		Matrizes(unsigned int, unsigned int);
+        void dimensoes(unsigned int, unsigned int);
+        unsigned int getQtdLinhas();
+        unsigned int getQtdColunas();
+        bool quadrada();
+        void setMatriz();
+        float detLaplace();
+        void imprime();
+        void imprimeFormatada();
+        void diminuirMatriz(unsigned int, unsigned int);
+};
+
+#endif
