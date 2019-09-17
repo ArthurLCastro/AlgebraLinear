6c65879779f3ff9d6c40d73c835374c260ea01da
Arthur Castro <arthur.castro07@gmail.com>
2019-09-16 21:05:05 -0300

    Preparacao dos codigos para futura implementacao do teste de dependencia linear

--- Vetores/Vetores.cpp
+++ Vetores/Vetores.cpp
@@ -47,25 +47,82 @@ float Vetores::getValor(unsigned int indice){
 //===================================== MAIN() =====================================
 unsigned int elementos;
 
-Vetores meuVetor;
+void apresentacao();
+void cabecalho();
 
+Vetores vetorU;
+Vetores vetorV;
+
+// Função Principal
 int main() {
-    cout << "\n\xAF Insira a quantidade de elementos do Vetor: ";
-    cin >> elementos; 
-    meuVetor.dimensao(elementos);
+    apresentacao();
 
+    cabecalho();
+    cout << "\n\t\xAF Insira a quantidade de elementos dos Vetores \"u\" e \"v\": ";
+    cin >> elementos; 
+    vetorU.dimensao(elementos);
+    vetorV.dimensao(elementos);
+    cout << "\n\t";
+    system("pause");
+
+    cabecalho();
+    cout << "\n\t\xAF Atribua valores ao vetor \"u\": \n";
     for (int indice=0; indice<elementos; indice++){
         float valor;
 
-        cout << "\t> a(" << indice+1 << "): ";
+        cout << "\t\t> u(" << indice+1 << "): ";
         cin >> valor;
-        meuVetor.setVetor(indice, valor);
+        vetorU.setVetor(indice, valor);
+    }
+    cout << "\n\t\xAF Atribua valores ao vetor \"v\": \n";
+    for (int indice=0; indice<elementos; indice++){
+        float valor;
+
+        cout << "\t\t> v(" << indice+1 << "): ";
+        cin >> valor;
+        vetorV.setVetor(indice, valor);
     }
 
+    cabecalho();
+    cout << "\n\t\xAF Os vetores\n\n";
     for(int indice=0; indice<elementos; indice++){
-        cout << "a(" << indice+1 << "): " << meuVetor.getValor(indice) << "\n";
+        cout << "\t\tu(" << indice+1 << "): " << vetorU.getValor(indice) << "\n";
     }
+    cout << "\n";
+    for(int indice=0; indice<elementos; indice++){
+        cout << "\t\tv(" << indice+1 << "): " << vetorV.getValor(indice) << "\n";
+    }
+
+    cout << "\n\t   sao " << "dependencia\n\n\t";
 
     system("pause");
     return 0;
+}
+
+// Funções Auxiliares
+void apresentacao(){
+    system("cls");
+    cout << "================================================================================\n";
+    cout << "|                                                                              |\n";
+    cout << "|                                                                              |\n";
+    cout << "|                        PROJETO RELACAO DE DEPENDENCIA                        |\n";
+    cout << "|                                                                              |\n";
+    cout << "|                     Verifica se dois vetores sao LI ou LD                    |\n";
+    cout << "|                                                                              |\n";
+    cout << "|                   ----------------------------------------                   |\n";
+    cout << "|                                                                              |\n";
+    cout << "|                            Arthur Lima de Castro                             |\n";
+    cout << "|                              Setembro de 2019                                |\n";
+    cout << "|                           Algebra Linear - POLI/UPE                          |\n";
+    cout << "|                     Prof.: Paulo Hugo Espirito Santo Lima                    |\n";
+    cout << "|                                                                              |\n";
+    cout << "================================================================================\n\n";
+    system("pause");
+}
+
+void cabecalho(){
+    system("cls");
+    cout << "================================================================================\n";
+    cout << "|                        PROJETO RELACAO DE DEPENDENCIA                        |\n";
+    cout << "================================================================================\n\n";
 }
